c8805fe858ccf1a1963bd78f538f42dd4abb7bcd
khaledhazime <hazimekhaled01@gmail.com>
2023-05-10 19:39:02 -0300

    feat(FakeNewsSimulatorTimer.java): add timer to simulate fake news spread
    Add a timer to simulate the spread of fake news in the world. The timer runs for a maximum of 2 ticks and updates the world state by calling the `desenhaMundo()` method and getting the adjacent people for each person in the world.

--- src/main/java/Simulation/FakeNewsSimulatorTimer.java
+++ src/main/java/Simulation/FakeNewsSimulatorTimer.java
@@ -1,2 +1,32 @@
-package Simulation;public class FakeNewsSimulatorTimer {
+package Simulation;
+
+import Mundo.Mundo;
+import Pessoa.Pessoa;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FakeNewsSimulatorTimer extends TimerTask {
+
+    private Mundo mundo;
+    private int tickCount = 0;
+    private final int MAX_TICKS = 2; // define a quantidade de ticks a serem executados
+
+    public FakeNewsSimulatorTimer(Mundo mundo) {
+        this.mundo = mundo;
+    }
+
+    @Override
+    public void run() {
+        mundo.desenhaMundo();
+        for(Pessoa pessoa : mundo.getMapaPessoas()){
+            if (pessoa!=null){
+                mundo.getPessoasAdjacentes(pessoa, false);
+            }
+        }
+        tickCount++;
+        if (tickCount >= MAX_TICKS) { // encerra o loop quando atinge o número máximo de ticks
+            this.cancel();
+        }
+    }
+
 }

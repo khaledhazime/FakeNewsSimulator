bc5d7955d15313e2dc215aac2dd5ffcfdc4687db
khaledhazime <hazimekhaled01@gmail.com>
2023-05-10 00:42:08 -0300

    feat(PessoaBemInformada.java): add PessoaBemInformada class that extends Pessoa and implements IMovable interface feat(IMovable.java): add IMovable interface with move() method signature

---
+++ src/main/java/Pessoa/PessoaBemInformada.java
@@ -0,0 +1,50 @@
+package Pessoa;
+
+/**
+ *
+ * @author khaledhazime
+ */
+public class PessoaBemInformada extends Pessoa implements IMovable {
+
+    public PessoaBemInformada(int x, int y, int cor, String whatsappID) {
+        super(x, y, cor, whatsappID);
+        this.setCor(2);
+    }
+
+
+    @Override
+    public void move() {
+        Random random = new Random();
+        int direction = random.nextInt(4);
+        switch (direction) {
+            case 0 -> {
+                if (this.getX() < Mundo.LARGURA - 1) {
+                    this.setX(this.getX() + 1);
+                } else {
+                    this.setX(0);
+                }
+            }
+            case 1 -> {
+                if (this.getX() > 0) {
+                    this.setX(this.getX() - 1);
+                } else {
+                    this.setX(Mundo.LARGURA - 1);
+                }
+            }
+            case 2 -> {
+                if (this.getY() < Mundo.ALTURA - 1) {
+                    this.setY(this.getY() + 1);
+                } else {
+                    this.setY(0);
+                }
+            }
+            case 3 -> {
+                if (this.getY() > 0) {
+                    this.setY(this.getY() - 1);
+                } else {
+                    this.setY(Mundo.ALTURA - 1);
+                }
+            }
+        }
+    }
+}

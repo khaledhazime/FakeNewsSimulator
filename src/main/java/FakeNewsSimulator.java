9dd898204242546064d330d3535bf7030a22eb7f
khaledhazime <hazimekhaled01@gmail.com>
2023-05-10 00:32:00 -0300

    feat: add pom.xml and FakeNewsSimulator.java files
    Add pom.xml file with project information and dependencies. Add FakeNewsSimulator.java file with a simple "Hello World!" message.

---
+++ src/main/java/FakeNewsSimulator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author khaledhazime
+ */
+public class FakeNewsSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

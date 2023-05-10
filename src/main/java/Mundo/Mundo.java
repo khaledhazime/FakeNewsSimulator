d0bae2f9727940c8849b0ef9a1e8f49be1f1bda4
khaledhazime <hazimekhaled01@gmail.com>
2023-05-10 19:13:13 -0300

    feat(Mundo.java): add MeioComunicacaoConfiavel class and methods to create and draw it on the map
    The MeioComunicacaoConfiavel class is created to represent a reliable communication
    medium. The method criaMeioComunicacaoConfiavel creates a new instance of the
    MeioComunicacaoConfiavel class and sets its color and coordinates. The method
    desenhaMeioComunicacaoConfiavel draws the MeioComunicacaoConfiavel on the map.
    
    The desenhaMundo method was updated to create and draw the MeioComunicacaoConfiavel
    on the map. The method getPessoasAdjacentes was also updated to receive a boolean
    parameter to indicate whether to print the contacts or not.

--- src/main/java/Mundo/Mundo.java
+++ src/main/java/Mundo/Mundo.java
@@ -2,7 +2,10 @@ package Mundo;
 
 import Pessoa.Pessoa;
 import Colors.Colors;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -36,20 +39,51 @@ public class Mundo {
         }
     }
 
+    public void criaMeioComunicacaoConfiavel(MeioComunicacaoConfiavel meioComunicacaoConfiavel) {
+        int x = (int) (Math.random() * LARGURA);
+        int y = (int) (Math.random() * ALTURA);
+        ArrayList<int[]> coordenadas = new ArrayList<int[]>();
+        int[] coordenadaInicial = {x, y};
+        coordenadas.add(coordenadaInicial);
+
+        // adiciona coordenadas adjacentes
+        int[][] deltas = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}; // cima, baixo, esquerda, direita
+        for (int[] delta : deltas) {
+            int novoX = x + delta[0];
+            int novoY = y + delta[1];
+            if (novoX >= 0 && novoX < LARGURA && novoY >= 0 && novoY < ALTURA) {
+                int[] novaCoordenada = {novoX, novoY};
+                coordenadas.add(novaCoordenada);
+            }
+        }
+
+
+        meioComunicacaoConfiavel.setCor(1);
+        meioComunicacaoConfiavel.setCoordenadas(coordenadas);
+    }
+
+    public void desenhaMeioComunicacaoConfiavel(MeioComunicacaoConfiavel meioComunicacaoConfiavel) {
+        for (int[] coordenada : meioComunicacaoConfiavel.getCoordenadas()) {
+            mapa[coordenada[0]][coordenada[1]] = 4;
+        }
+    }
+
+
     public void desenhaMundo() {
+        System.out.println("Desenhando mundo...");
+        MeioComunicacaoConfiavel meioComunicacaoConfiavel = new MeioComunicacaoConfiavel();
+
+        criaMeioComunicacaoConfiavel(meioComunicacaoConfiavel);
+        desenhaMeioComunicacaoConfiavel(meioComunicacaoConfiavel);
+
         for (int i = 0; i < LARGURA; i++) {
             for (int j = 0; j < ALTURA; j++) {
                 switch (mapa[i][j]) {
-                    case 0 -> System.out.print(Colors.WHITE.getColorCode() + "0");
+                    case 0 -> System.out.print(Colors.getColor(0) + "0");
                     case 1 ->{
-                        int cor = getPessoa(i, j).getCor();
-                        if(cor == 1) {
-                            System.out.print(Colors.BLUE.getColorCode() + "1");
-                        } else if(cor == 2) {
-                            System.out.print(Colors.RED.getColorCode() + "2");
-                        }
-
+                        System.out.print(Colors.getColor(getPessoa(i, j).getCor()) + "P");
                     }
+                    case 4 -> System.out.print(Colors.getColor(4) + "M");
                 }
             }
             System.out.println();
@@ -71,7 +105,7 @@ public class Mundo {
         return null;
     }
 
-    public void getPessoasAdjacentes(Pessoa pessoa){
+    public void getPessoasAdjacentes(Pessoa pessoa, boolean printaContatos){
         int x = pessoa.getX();
         int y = pessoa.getY();
         Pessoa pessoaAdjacente;
@@ -91,7 +125,7 @@ public class Mundo {
             pessoaAdjacente = getPessoa(x, y + 1);
             pessoa.addContatos(pessoaAdjacente);
         }
-        if (pessoa.getAgendaContatos().size() > 0){
+        if (pessoa.getAgendaContatos().size() > 0 && printaContatos) {
             System.out.println(pessoa.getWhatsappID() + " tem " + pessoa.getAgendaContatos().size() + " contatos");
             System.out.print("Contatos: [");
             for (Pessoa contato : pessoa.getAgendaContatos()) {

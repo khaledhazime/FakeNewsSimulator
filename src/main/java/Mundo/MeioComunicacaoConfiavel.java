be7600e27e4a3694533d04d8e76dd6b428ed82c7
khaledhazime <hazimekhaled01@gmail.com>
2023-05-10 19:12:50 -0300

    feat(MeioComunicacaoConfiavel.java): add cor and coordenadas properties to MeioComunicacaoConfiavel class and respective getters and setters

--- src/main/java/Mundo/MeioComunicacaoConfiavel.java
+++ src/main/java/Mundo/MeioComunicacaoConfiavel.java
@@ -1,2 +1,30 @@
-package Mundo;public class MeioComunicacaoConfiavel {
+package Mundo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeioComunicacaoConfiavel {
+    private int cor;
+    private List<int[]> coordenadas;
+
+    public MeioComunicacaoConfiavel(){
+        this.cor = 1;
+        this.coordenadas = new ArrayList<int[]>();
+    }
+
+    public int getCor() {
+        return cor;
+    }
+
+    public void setCor(int cor) {
+        this.cor = cor;
+    }
+
+    public List<int[]> getCoordenadas() {
+        return coordenadas;
+    }
+
+    public void setCoordenadas(List<int[]> coordenadas) {
+        this.coordenadas = coordenadas;
+    }
 }
